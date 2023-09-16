6dfa3c6cb5618c8b0680b6783cf261c0e11dfe11
Nikolai Yusev <>
2023-09-16 22:18:01 +0300

    add debug

--- webapp/src/hooks/permissions.tsx
+++ webapp/src/hooks/permissions.tsx
@@ -8,8 +8,8 @@ import {Permission} from '../constants'
 import {MemberRole} from '../blocks/board'
 
 export const useHasPermissions = (teamId: string, boardId: string, permissions: Permission[]): boolean => {
-    console.log("useHasPermissions");
     if (!boardId || !teamId) {
+        console.log("!boardId || !teamId false");
         return false
     }
 
@@ -17,38 +17,38 @@ export const useHasPermissions = (teamId: string, boardId: string, permissions: 
     const board = useAppSelector(getBoard(boardId))
 
     if (!board) {
+        console.log("!boardId false");
         return false
     }
 
     if (!member) {
+        console.log("!member false");
         return false
     }
 
     const adminPermissions = [Permission.ManageBoardType, Permission.DeleteBoard, Permission.ShareBoard, Permission.ManageBoardRoles, Permission.DeleteOthersComments]
-    const editorPermissions = [Permission.ManageBoardCards, Permission.ManageBoardProperties]
-    const commenterPermissions = [Permission.CommentBoardCards]
+    const editorPermissions = [Permission.ManageBoardCards, Permission.ManageBoardProperties, Permission.ManageBoardRoles]
+    const commenterPermissions = [Permission.CommentBoardCards, Permission.ManageBoardRoles]
     const viewerPermissions = [Permission.ViewBoard]
-    console.log("start loop");
     for (const permission of permissions) {
-        console.log("permission");
-        console.log(permission);
         if (adminPermissions.includes(permission) && member.schemeAdmin) {
-            console.log("adminPermissions");
+            console.log("adminPermissions: TRUE");
             return true
         }
         if (editorPermissions.includes(permission) && (member.schemeAdmin || member.schemeEditor || board.minimumRole === MemberRole.Editor)) {
-            console.log("editorPermissions");
+            console.log("editorPermissions: TRUE");
             return true
         }
         if (commenterPermissions.includes(permission) && (member.schemeAdmin || member.schemeEditor || member.schemeCommenter || board.minimumRole === MemberRole.Commenter || board.minimumRole === MemberRole.Editor)) {
-            console.log("commenterPermissions");
+            console.log("commenterPermissions: TRUE");
             return true
         }
         if (viewerPermissions.includes(permission) && (member.schemeAdmin || member.schemeEditor || member.schemeCommenter || member.schemeViewer || board.minimumRole === MemberRole.Viewer || board.minimumRole === MemberRole.Commenter || board.minimumRole === MemberRole.Editor)) {
-            console.log("viewerPermissions");
+            console.log("viewerPermissions: TRUE");
             return true
         }
     }
+    console.log("after loop false");
     return false
 }
 
